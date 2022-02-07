fd8533a4512cc280ec2468650a49d93e5aad51f9
Ander1405 <cardonaac389@gmail.com>
2022-02-06 21:25:23 -0500

    feature:validation_form_request_for_products

---
+++ app/Http/Requests/Admin/Products/StoreProductRequest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Requests\Admin\Products;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class StoreProductRequest extends FormRequest
+{
+
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'min:5', 'max:50'],
+            'price' => ['required', 'integer', 'min:1'],
+            'description' => ['required', 'min:5', 'max:250'],
+            'image' => ['required',]
+        ];
+    }
+
+}
---
+++ app/Http/Requests/Admin/Products/UpdateProductRequest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Requests\Admin\Products;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateProductRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+
+    public function rules(): array
+    {
+        return [
+            'name'=>['required'],
+            'description'=>['required'],
+            'price'=>['required'],
+            'image'=>['required'],
+        ];
+    }
+}
