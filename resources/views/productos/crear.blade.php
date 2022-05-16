6fe6e1ae3a93679aeb956fe1c301749e3e72cdb6
Ander1405 <cardonaac389@gmail.com>
2022-05-16 06:30:48 -0500

    feature: chsnges for views of produtcs

--- resources/views/productos/crear.blade.php
+++ resources/views/productos/crear.blade.php
@@ -1,51 +1,60 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Crear Productos') }}
+        <h2 class="text-center font-semibold text-4xl text-gray-800 leading-tight">
+            {{trans('Create New Product')}}
         </h2>
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+            <div class="bg-gray-300 overflow-hidden shadow-xl sm:rounded-lg">
 
                 <form action="{{ route('productos.store') }}" method="POST" enctype="multipart/form-data">
                     @csrf
                     <div class="grid grid-cols-1 md:grid-cols-2 gap-5 md:gap-8 mt-5 mx-7">
                         <div class="grid grid-cols-1">
-                            <label class="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">Nombre:</label>
+                            <label class="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">{{ trans('Name') }}:</label>
                             <input name="name" class="py-2 px-3 rounded-lg border-2 border-purple-300 mt-1 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" type="text" required/>
                         </div>
                         <div class="grid grid-cols-1">
-                            <label class="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">stock:</label>
+                            <label class="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold" for="category_id">{{ trans('Category') }}:</label>
+                            <select class="form-control rounded-lg m-2" name="category_id" id="category_id">
+                                <option disabled selected>Seleccione categoria</option>
+                                @foreach ($categories as $category)
+                                    <option value="{{$category->id}}">{{$category->name}}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                        <div class="grid grid-cols-1">
+                            <label class="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">{{ trans('Quantity') }}:</label>
                             <input name="stock" class="py-2 px-3 rounded-lg border-2 border-purple-300 mt-1 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" type="text" required/>
                         </div>
                         <div class="grid grid-cols-1">
-                            <label class="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">Precio:</label>
+                            <label class="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">{{ trans('Price') }}:</label>
                             <input name="price" class="py-2 px-3 rounded-lg border-2 border-purple-300 mt-1 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" type="text" required/>
                         </div>
                         <div class="grid grid-cols-1">
-                            <label class="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">Descripción:</label>
+                            <label class="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">{{ trans('Description') }}:</label>
                             <input name="description" class="py-2 px-3 rounded-lg border-2 border-purple-300 mt-1 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" type="text" required/>
                         </div>
                         <div class="grid grid-cols-1">
-                            <label class="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">storage:</label>
+                            <label class="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">{{ trans('Storage') }}:</label>
                             <input name="storage" class="py-2 px-3 rounded-lg border-2 border-purple-300 mt-1 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" type="text" required/>
                         </div>
                         <div class="grid grid-cols-1">
-                            <label class="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">ram:</label>
+                            <label class="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">{{ trans('Ram') }}:</label>
                             <input name="ram" class="py-2 px-3 rounded-lg border-2 border-purple-300 mt-1 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" type="text" required/>
                         </div>
                         <div class="grid grid-cols-1">
-                            <label class="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">graph:</label>
+                            <label class="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">{{ trans('Graph') }}:</label>
                             <input name="graph" class="py-2 px-3 rounded-lg border-2 border-purple-300 mt-1 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" type="text" required/>
                         </div>
                         <div class="grid grid-cols-1">
-                            <label class="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">brand:</label>
+                            <label class="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">{{ trans('Brand') }}:</label>
                             <input name="brand" class="py-2 px-3 rounded-lg border-2 border-purple-300 mt-1 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" type="text" required/>
                         </div>
                         <div class="grid grid-cols-1">
-                            <label class="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">processor:</label>
+                            <label class="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">{{ trans('Processor') }}:</label>
                             <input name="processor" class="py-2 px-3 rounded-lg border-2 border-purple-300 mt-1 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" type="text" required/>
                         </div>
                     </div>
@@ -56,12 +65,12 @@
                     </div>
 
                     <div class="grid grid-cols-1 mt-5 mx-7">
-                        <label class="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold mb-1">Subir Imagen</label>
+                        <label class="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold mb-1">{{ trans('Upload image') }}</label>
                         <div class='flex items-center justify-center w-full'>
                             <label class='flex flex-col border-4 border-dashed w-full h-32 hover:bg-gray-100 hover:border-purple-300 group'>
                                 <div class='flex flex-col items-center justify-center pt-7'>
                                     <svg class="w-10 h-10 text-purple-400 group-hover:text-purple-600" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"></path></svg>
-                                    <p class='text-sm text-gray-400 group-hover:text-purple-600 pt-1 tracking-wider'>Seleccione la imagen</p>
+                                    <p class='text-sm text-gray-400 group-hover:text-purple-600 pt-1 tracking-wider'>{{ trans('Select image') }}</p>
                                 </div>
                                 <input name="image" id="image" type='file' class="hidden" />
                             </label>
@@ -69,8 +78,8 @@
                     </div>
 
                     <div class='flex items-center justify-center  md:gap-8 gap-4 pt-5 pb-5'>
-                        <a href="{{ route('productos.index') }}" class='w-auto bg-gray-500 hover:bg-gray-700 rounded-lg shadow-xl font-medium text-white px-4 py-2'>Cancelar</a>
-                        <button type="submit" class='w-auto bg-purple-500 hover:bg-purple-700 rounded-lg shadow-xl font-medium text-white px-4 py-2'>Guardar</button>
+                        <a href="{{ route('productos.index') }}" class='w-auto bg-gray-500 hover:bg-gray-700 rounded-lg shadow-xl font-medium text-white px-4 py-2'>{{ trans('Cancel') }}</a>
+                        <button type="submit" class='w-auto bg-purple-500 hover:bg-purple-700 rounded-lg shadow-xl font-medium text-white px-4 py-2'>{{ trans('Save') }}</button>
                     </div>
                 </form>
 
